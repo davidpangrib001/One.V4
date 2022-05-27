24161091db2e563e29178a16b8fe57465e999027
davidpangrib001 <davidpangrib001@gmail.com>
2022-05-27 19:02:45 +0700

    namasc

---
+++ plugins/ytv.js
@@ -0,0 +1,31 @@
+let limit = 45
+let fetch = require('node-fetch')
+const { servers, ytv } = require('../lib/y2mate')
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `Mana Link Video YouTube-nya Bang??\n\nContoh: .ytmp4 https://youtube.com/watch?v=doFVaVZrIeg`
+  let chat = global.db.data.chats[m.chat]
+  let server = (args[1] || servers[0]).toLowerCase()
+  try {
+    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
+    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+    m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
+    let _thumb = {}
+    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+    catch (e) { }
+    if (!isLimit) conn.sendFile(m.chat, dl_link, '', `
+*Judul:* ${title}
+*Ukuran File:* ${filesizeF}
+  `.trim(), m, 0, {
+      ..._thumb,
+      asDocument: chat.useDocument
+    })
+  } catch (e) {
+    return await conn.sendButton(m.chat, 'Ada Yang Error Dengan Server Download\n\n_Kadang :v_', '', 'Coba Lagi Aja..', `${usedPrefix + command} ${args[0]}`)
+  }
+}
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
+handler.tags = ['downloader']
+handler.command = /^yt(v|mp4)?$/i
+handler.limit = true
+
+module.exports = handler
